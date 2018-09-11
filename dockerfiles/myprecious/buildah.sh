a25f876b70e890f2f8101811ea6a0f9ea190e38f
Micah Abbott <miabbott@redhat.com>
2018-09-11 12:34:18 -0400

    multiple changes to support rpm-ostree devel, etc

--- dockerfiles/myprecious/buildah.sh
+++ dockerfiles/myprecious/buildah.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -xeou pipefail
 
+if [ $# -eq 0 ]; then
+  echo "Must supply value for releasever"
+  exit 1
+fi
+releasever=$1; shift
+
 # create base container
-ctr=$(buildah from registry.fedoraproject.org/fedora:27)
+ctr=$(buildah from registry.fedoraproject.org/fedora:$releasever)
 
 # mount container filesystem
 mp=$(buildah mount $ctr)
+
+dnf_cmd() {
+  dnf -y --installroot $mp --releasever $releasever $@
+}
 
 # set the maintainer label
 buildah config --label maintainer="Micah Abbott <miabbott@redhat.com>" $ctr
@@ -16,21 +26,37 @@ curl -L -o $mp/etc/yum.repos.d/qa-tools.repo http://liver.brq.redhat.com/repo/qa
 
 # reinstall all pkgs with docs
 sed -i '/tsflags=nodocs/d' $mp/etc/dnf/dnf.conf
-dnf -y reinstall --disablerepo=beaker-client --disablerepo=qa-tools --installroot $mp --releasever 27 '*'
+dnf -y --installroot $mp --releasever $releasever --disablerepo=beaker-client --disablerepo=qa-tools reinstall '*'
+
+# install tools needed for building ostree/rpm-ostree stack
+dnf_cmd install @buildsys-build dnf-plugins-core
+dnf_cmd builddep ostree rpm-ostree
 
 # install the rest
-dnf -y install --installroot $mp --releasever 27 \
+dnf_cmd install \
                    awscli \
                    beaker-client \
                    beaker-redhat \
+                   btrfs-progs-devel\
+                   conmon \
                    conserver-client \
+                   createrepo_c \
                    cyrus-sasl-gssapi \
+                   device-mapper-devel \
                    fuse \
                    gcc \
                    git \
                    git-review \
+                   glib2-devel \
+                   glibc-static \
                    golang \
+                   golang-github-cpuguy83-go-md2man \
+                   gpgme-devel \
                    iputils \
+                   libassuan-devel \
+                   libgpg-error-devel \
+                   libseccomp-devel \
+                   libselinux-devel \
                    jq \
                    man \
                    python-qpid-messaging \
@@ -43,10 +69,12 @@ dnf -y install --installroot $mp --releasever 27 \
                    rsync \
                    sshpass \
                    sudo \
+                   tmux \
                    vim
 
+
 # clean up
-dnf clean all --installroot $mp
+dnf_cmd clean all
 
 # setup sudoers
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> $mp/etc/sudoers
@@ -59,7 +87,7 @@ chroot $mp bash -c "/usr/sbin/useradd --groups wheel --uid 1000 miabbott"
 buildah config --user miabbott $ctr
 
 # commit the image
-buildah commit $ctr miabbott/myprecious:buildah27
+buildah commit $ctr miabbott/myprecious:$releasever
 
 # unmount and remove the container
 buildah unmount $ctr
