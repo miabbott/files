0e9ec9427888063db4c52544cd0e493ab800f754
Micah Abbott <miabbott@redhat.com>
2019-01-09 15:56:20 -0500

    use dumb-init to catc SIGTERM; turn on bash strict mode

--- dockerfiles/jq/buildah.sh
+++ dockerfiles/jq/buildah.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -xeou pipefail
 
 JQ_VERSION="1.6"
 
@@ -7,6 +8,10 @@ ctr=$(buildah from scratch)
 mp=$(buildah mount $ctr)
 
 mkdir -p $mp/usr/bin
+wget --no-check-certificate https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64 -O /tmp/dumb-init
+cp /tmp/dumb-init $mp/usr/bin/dumb-init
+chmod +x $mp/usr/bin/dumb-init
+rm -f /tmp/dumb-init
 wget --no-check-certificate https://raw.githubusercontent.com/stedolan/jq/master/sig/jq-release.key -O /tmp/jq-release.key
 wget --no-check-certificate https://raw.githubusercontent.com/stedolan/jq/master/sig/v${JQ_VERSION}/jq-linux64.asc -O /tmp/jq-linux64.asc
 wget --no-check-certificate https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64 -O /tmp/jq-linux64
@@ -18,7 +23,7 @@ rm -f /tmp/jq-release.key
 rm -f /tmp/jq-linux64.asc
 rm -f /tmp/jq-linux64
 
-buildah config --entrypoint '["/usr/bin/jq"]' $ctr
+buildah config --entrypoint '["/usr/bin/dumb-init", "/usr/bin/jq"]' $ctr
 
 buildah commit $ctr miabbott/jq
 
